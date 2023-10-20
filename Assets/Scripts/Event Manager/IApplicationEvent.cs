2e2b9c719ddca8eb7008ee4975bf9706b73fef0f
Dani <dagugo.96@gmail.com>
2023-10-20 15:29:28 -0500

    [Add]: Methods added to the IApplicationEvent interface

--- Assets/Scripts/Event Manager/IApplicationEvent.cs
+++ Assets/Scripts/Event Manager/IApplicationEvent.cs
@@ -5,4 +5,15 @@ public interface IApplicationEvent
     void InvokeString(string value);
     void InvokeDoubleString(string value1, string value2);
     void InvokeDoubleInt(int value1, int value2);
+    void InvokeFloat(float value);
+    void InvokeDoubleFloat(float value1, float value2);
+    void InvokeTripleFloat(float value1, float value2, float value3);
+    void InvokeBool(bool value);
+    void InvokeDoubleBool(bool value1, bool value2);
+    void InvokeTripleBool(bool value1, bool value2);
+    void InvokeBoolString(bool value1, string value2);
+    void InvokeObject(object value);
+    void InvokeGameObject(UnityEngine.GameObject value);
+    void InvokeTwoGameObject(UnityEngine.GameObject value1, UnityEngine.GameObject value2);
+    void InvokeDouble(double value);
 }

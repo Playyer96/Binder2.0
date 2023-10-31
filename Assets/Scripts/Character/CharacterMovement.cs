ac46b6b631b6ac6aa6faa7f329ecbd5a9c8a6160
Dani <dagugo.96@gmail.com>
2023-10-31 16:19:52 -0500

    [Mod]: Character movement changes

--- Assets/Scripts/Character/CharacterMovement.cs
+++ Assets/Scripts/Character/CharacterMovement.cs
@@ -5,12 +5,11 @@ public class CharacterMovement : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5.0f;
     [SerializeField] private float rotationSpeed = 5.0f;
-    [SerializeField] private float offsetDistance = 1.0f; // Desired distance in front of the character.
+    [SerializeField] private float avoidanceRadius = 0.2f;
 
     private Rigidbody2D rb;
-    private Vector3 targetPosition;
+    private Vector2 targetPosition;
     private bool isMoving;
-    private Vector3 offset; // Declare the offset variable at the class level.
 
     private void Awake()
     {
@@ -26,11 +25,7 @@ public class CharacterMovement : MonoBehaviour
     private void Update()
     {
         targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        targetPosition.z = transform.position.z;
         isMoving = true;
-
-        // Calculate the offset based on the character's local forward direction and desired distance.
-        offset = transform.TransformDirection(Vector3.right) * offsetDistance;
     }
 
     private void FixedUpdate()
@@ -38,29 +33,55 @@ public class CharacterMovement : MonoBehaviour
         if (isMoving)
         {
             MoveCharacter();
+            RotateCharacter();
         }
+    }
+
+    private void OnDrawGizmos()
+    {
+        // Draw the avoidance radius in the scene view.
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, avoidanceRadius);
     }
 
     private void MoveCharacter()
     {
-        Vector3 direction = (targetPosition - (transform.position + offset));
-        if (direction.magnitude > 0.1f)
+        Vector2 direction = targetPosition - rb.position;
+
+        // Check for obstacles within the avoidanceRadius using OverlapCircle.
+        Collider2D[] hits = Physics2D.OverlapCircleAll(rb.position, avoidanceRadius);
+
+        bool shouldMove = true;
+
+        foreach (var hit in hits)
         {
-            Vector2 newPosition = Vector2.Lerp(transform.position, targetPosition - offset, moveSpeed * Time.fixedDeltaTime);
+            if (hit.gameObject != gameObject)
+            {
+                // An obstacle is detected, stop moving.
+                shouldMove = false;
+                break;
+            }
+        }
+
+        if (shouldMove)
+        {
+            Vector2 newPosition = Vector2.MoveTowards(rb.position, targetPosition, moveSpeed * Time.fixedDeltaTime);
             rb.MovePosition(newPosition);
         }
         else
         {
             isMoving = false;
         }
-        RotateCharacter();
     }
 
     private void RotateCharacter()
     {
-        Vector2 direction = (targetPosition - (transform.position + offset)).normalized;
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationSpeed * Time.fixedDeltaTime);
+        Vector2 direction = targetPosition - rb.position;
+        if (direction != Vector2.zero)
+        {
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationSpeed * Time.fixedDeltaTime);
+        }
     }
 }
